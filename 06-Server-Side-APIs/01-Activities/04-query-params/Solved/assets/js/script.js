5f51dcccbc2f5dfb5a8295371a53cfb1a68f8f00
Kevin Ferguson <kfergrocks@gmail.com>
2021-05-19 20:59:32 -0400

    updates

---
+++ 06-Server-Side-APIs/01-Activities/04-query-params/Solved/assets/js/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+  var searchTerm = document.querySelector("#searchTerm").value;
+  // Create a variable to hold the value of rating
+  var rating = document.querySelector("#rating").value;
+  fetch(
+    "https://api.giphy.com/v1/gifs/search?q=" +
+      searchTerm +
+      // Add the rating parameter
+      "&rating=" +
+      rating +
+      "&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=10"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.data[0]);
+      var responseContainerEl = document.querySelector("#response-container");
+      responseContainerEl.innerHTML = "";
+      for (var i = 0; i < 10; i++) {
+        var gifImg = document.createElement("img");
+        gifImg.setAttribute("src", response.data[i].images.fixed_height.url);
+        responseContainerEl.appendChild(gifImg);
+      }
+    });
+}
