f43ef7745b22feb11d2a14036942fa509e0a3c9f
Kevin Ferguson <kfergrocks@gmail.com>
2021-06-09 20:54:35 -0400

    updates

--- 09-NodeJS/02-Challenge/Develop/index.js
+++ 09-NodeJS/02-Challenge/Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+// require inquirer
+// require generateMardown
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +9,11 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // use inquirer to ask questions (activity 2 from Monday)
+  // call generateMarkdown function which will return a string
+  // call writeToFile function pass to it a file name and the string returned by the generateMarkdown function
+}
 
 // Function call to initialize app
 init();
