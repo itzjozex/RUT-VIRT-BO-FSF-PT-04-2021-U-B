16734c1593f140498f365bd4ead62ef1b318b45f
Kevin Ferguson <kfergrocks@gmail.com>
2021-09-09 20:21:12 -0400

    updates

--- 22-State/01-Activities/06-Global-State-Refactor with Redux/Unsolved/src/App.js
+++ 22-State/01-Activities/06-Global-State-Refactor with Redux/Unsolved/src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
-import { Provider } from 'react-redux';
-import store from './utils/store';
+import { StoreProvider } from './utils/GlobalState';
+
+import Header from './components/Header';
+import BookList from './pages/BookList';
+import Detail from './pages/Detail';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="m-5">
-      <Provider store={store}>
-        <h4>Here is the initialState:</h4>
-        <div className="container">{JSON.stringify(store.getState())}</div>
-      </Provider>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <StoreProvider>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={BookList} />
+                <Route exact path="/book/:bookId" component={Detail} />
+                <Route render={() => <h1>404! Wrong Page</h1>} />
+              </Switch>
+            </div>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- 22-State/01-Activities/06-Global-State-Refactor with Redux/Unsolved/src/utils/reducers.js
+++ 22-State/01-Activities/06-Global-State-Refactor with Redux/Unsolved/src/utils/reducers.js
@@ -1,26 +1,24 @@
+import { useReducer } from 'react';
 import { UPDATE_BOOKS, UPDATE_CURRENT_BOOK } from './actions';
 
-const initialState = {
-  books: [],
-  currentBook: {}
-};
-
-export const reducer = (state = initialState, action) => {
+export const reducer = (state, action) => {
   switch (action.type) {
     case UPDATE_BOOKS:
       return {
         ...state,
-        books: [...action.books]
+        books: [...action.books],
       };
 
     case UPDATE_CURRENT_BOOK:
       return {
         ...state,
-        currentBook: { ...action.currentBook }
+        currentBook: { ...action.currentBook },
       };
     default:
       return state;
   }
 };
 
-export default reducer;
+export function useBookReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
